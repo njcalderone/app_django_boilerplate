b05d8e14bddd27634b742a9fdb022d5b68defc9e
njcalderone <112025653+njcalderone@users.noreply.github.com>
2022-11-03 12:54:24 -0500

    New Branch

--- App_Data_Table/views2.py
+++ App_Data_Table/views2.py
@@ -5,7 +5,7 @@ import Project_Django_Boilerplate_GAP.views
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Project_Django_Boilerplate_GAP.settings')
 django.setup()
 
-from App_Data_Table.models import Member
+from App_Data_Table.models import Member2
 from django.shortcuts import render, redirect
 from decouple import config
 
@@ -21,13 +21,13 @@ ENCODED_REDIRECT_URI = config("ENCODED_REDIRECT_URI")
 
 # Create your views here.
 def index(request):
-    all_members = Member.objects.all()
+    all_members = Member2.objects.all()
     return render(request, 'datatables/index.html', {'all_members': all_members, 'base_url': Base_URL})
 
 
 def insert(request):
-    member = Member(firstname=request.POST['firstname'], lastname=request.POST['lastname'],
-                    address=request.POST['address'], phone=request.POST['phone'])
+    member = Member2(firstname=request.POST['firstname'], lastname=request.POST['lastname'],
+                    eyecolor=request.POST['eyecolor'], height=request.POST['height'])
     member.save()
     return redirect(Base_URL + 'datatables/index.html')
 
