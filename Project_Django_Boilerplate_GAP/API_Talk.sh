4e13b3a59a29b1ec555642784f9e625fe253f4e2
njcalderone <112025653+njcalderone@users.noreply.github.com>
2022-11-16 12:22:43 -0500

    Clean up artifacts

--- Project_Django_Boilerplate_GAP/API_Talk.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#users
-#curl -X GET 'http://localhost:8080/auth/admin/realms/default/users?username=user1&exact=true' -H "Content-Type: application/json" -H "Authorization: Bearer $(curl -d "client_id=admin-cli" -d "username=admin" -d "password=admin" -d "grant_type=password" "http://localhost:8080/auth/realms/master/protocol/openid-connect/token" |python3 -c "import sys, json; print(json.load(sys.stdin)['access_token'])")"
-#curl -X GET 'http://localhost:8080/auth/admin/realms/default/users/ac7c0c7c-5b8c-43ea-9c44-9526bcc0ddd6/role-mappings' -H "Content-Type: application/json" -H "Authorization: Bearer $(curl -d "client_id=admin-cli" -d "username=admin" -d "password=admin" -d "grant_type=password" "http://localhost:8080/auth/realms/master/protocol/openid-connect/token" |python3 -c "import sys, json; print(json.load(sys.stdin)['access_token'])")"
-user_name="user1"
-curl -X GET 'http://localhost:8080/auth/admin/realms/default/users?username='$user_name'&exact=true' -H "Content-Type: application/json" -H "Authorization: Bearer $(curl -d "client_id=admin-cli" -d "username=admin" -d "password=admin" -d "grant_type=password" "http://localhost:8080/auth/realms/master/protocol/openid-connect/token" |python3 -c "import sys, json; print(json.load(sys.stdin)['access_token'])")"
